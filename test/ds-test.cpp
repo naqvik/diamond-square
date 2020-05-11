162e8a159fa4768545576d356857f4920bab80af
Karim Naqvi <naqvik@uregina.ca>
2020-05-11 12:23:40 -0600

    Initial commit: doctest wired up correctly

---
+++ test/ds-test.cpp
@@ -0,0 +1,13 @@
+// -*- c++ -*-
+
+#include <cstdint>
+#include <iostream>
+
+#include "../src/ds.h"          // implementation
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_CASE( "" ) {
+    REQUIRE( 1 == 1 );
+}
