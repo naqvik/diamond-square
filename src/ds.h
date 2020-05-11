4a81d713f07ea317303697365f8cc6f58cbc1994
Karim Naqvi <naqvik@uregina.ca>
2020-05-11 17:53:36 -0600

    Pass test: read/write an array cell
    I've mimicked the standard 2D-array C notation by overloading [].  The
    Array class auto-manages the memory.

--- src/ds.h
+++ src/ds.h
@@ -1,12 +1,24 @@
 // -*- c++ -*-
 // Diamond-Square Implementation
+#include <cstdint>
+#include <memory>
+
 
 class Array {
 private:
-    int size_ = 0;
+    int const size_;
+    std::unique_ptr<uint8_t[]> arr_;
 public:
-    explicit Array(int sz) : size_(sz) {
+    explicit Array(int sz)
+        : size_(sz),
+          arr_{std::make_unique<uint8_t[]>(size_*size_)} {}
 
-    }
+    // allow array[r][c] notation in client
+    uint8_t *operator[](int row) { return row * size_ + arr_.get(); };
+
+    // allow array(r,c) notation in client
+    uint8_t &operator()(int row, int col) {
+        return arr_[row * size_ + col];
+    };
     int size() const { return size_; }
 };
